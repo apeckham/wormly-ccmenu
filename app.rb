e95e272c25dd6835a154f371f86d68218631ec77
Aaron Peckham <apeckham@local>
2012-04-26 15:11:55 -0700

    show wormly status in xml file

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'rack/cache'
 require 'sinatra'
+require File.dirname(__FILE__) + '/wormly'
 
 use Rack::Cache, verbose: true
 
 get '/cctray.xml' do
-  cache_control :public, :max_age => 10
-  erb :cctray
+  cache_control :public, :max_age => 120
+  erb :cctray, locals: {my_hosts: Wormly.my_hosts}
 end
