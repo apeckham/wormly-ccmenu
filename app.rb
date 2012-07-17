73f145d0daebfa827208fad6bfd5f7ce468c6a14
Aaron Peckham <apeckham@gmail.com>
2012-07-17 09:17:53 -0700

    absolutize wormly urls

--- app.rb
+++ app.rb
@@ -5,6 +5,6 @@ require File.dirname(__FILE__) + '/wormly'
 use Rack::Cache, verbose: true
 
 get '/cctray.xml' do
-  cache_control :public, :max_age => 120
+  cache_control :public, max_age: 120
   erb :cctray, locals: {my_hosts: Wormly.my_hosts}
 end
--- wormly.rb
+++ wormly.rb
@@ -1,12 +1,15 @@
 require 'mechanize'
 require 'nokogiri'
+require 'uri'
 
 class Wormly
+  BASE_URL = 'https://www.wormly.com/login'
+  
   def self.my_hosts
     raise "WORMLY_EMAIL and WORMLY_PASSWORD are required" unless ENV['WORMLY_EMAIL'] && ENV['WORMLY_PASSWORD']
     agent = Mechanize.new
 
-    page = agent.get 'https://www.wormly.com/login'
+    page = agent.get BASE_URL
     hosts_page = page.form_with(id: 'login') do |form|
       form.email = ENV['WORMLY_EMAIL']
       form.password = ENV['WORMLY_PASSWORD']
@@ -14,7 +17,11 @@ class Wormly
     
     doc = Nokogiri::HTML(hosts_page.body)
     doc.css("#hostlist tbody tr").map do |row|
-      {webUrl: row.at(".reportcol a")["href"], lastBuildStatus: row.at(".icon.pass") ? "Success" : "Failure", name: row.at(".textualtitle").text}
+      {
+        webUrl: URI.join(BASE_URL, row.at(".reportcol a")["href"]).to_s,
+        lastBuildStatus: row.at(".icon.pass") ? "Success" : "Failure",
+        name: row.at(".textualtitle").text
+      }
     end
   end
 end
